63de946a75a4e66b18c5b8cbb88259c1a6c694f6
vuehero18 <vuehero18@outlook.com>
2019-10-01 21:38:02 -0700

    Updates

---
+++ precache-manifest.62dbef62323940c58c363d404aaf76d7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "33db2f953c154ddd71085fe35a772d9e",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "ae6f1a613a7b50adbfb6",
+    "url": "/monsters-rolodex/static/css/main.d588b3e0.chunk.css"
+  },
+  {
+    "revision": "964ccf7ba0449dad8dff",
+    "url": "/monsters-rolodex/static/js/2.a20b2da9.chunk.js"
+  },
+  {
+    "revision": "ae6f1a613a7b50adbfb6",
+    "url": "/monsters-rolodex/static/js/main.561185f8.chunk.js"
+  },
+  {
+    "revision": "137f4aaae6459cb874ed",
+    "url": "/monsters-rolodex/static/js/runtime-main.2d5d4edd.js"
+  }
+]);
